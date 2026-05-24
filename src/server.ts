c24936735da69c7cc3e3a48ec5e09ea0c465f77f
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-05-24 15:29:56 +0900

    feat: express integrations

--- src/server.ts
+++ src/server.ts
@@ -2,21 +2,21 @@
 
 // MVC = MODEL VIEW CONTROLLER
 // Design(dizayn) pattern(naqsh): Middleware, Decotar
+// // CLUSTER => DATABASE => COLLECTION => DOCUMENT
 
 import dotenv from 'dotenv';
 dotenv.config();
-
-
-
-// // CLUSTER => DATABASE => COLLECTION => DOCUMENT
-
 import mongoose from "mongoose";
+import app from "./app";
 
 mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then((data) => {
     console.log("MongoDB connection succeed");
     const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function() {
+      console.log(`belgilangan portda yurgazildi: ${PORT}`);
+    })
   })
   .catch((err) => console.log("ERROR on connection MongoDB", err));
 
