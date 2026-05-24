464846115f6e2f25f7e8130e11942ea6fc4fcf53
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-05-24 13:42:38 +0900

    feat: integration of enviromental variables

--- src/server.ts
+++ src/server.ts
@@ -2,3 +2,9 @@
 
 // MVC = MODEL VIEW CONTROLLER
 // Design(dizayn) pattern(naqsh): Middleware, Decotar
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log("PORT:", process.env.PORT)
+console.log("PORT:", process.env.MONGO_URL)
