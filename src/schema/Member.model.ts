4ab914666024271f94751ffe72893f1f08928443
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-05-28 22:38:06 +0900

    fix: modified member.modul.ts

--- src/schema/Member.model.ts
+++ src/schema/Member.model.ts
@@ -1,7 +1,7 @@
 import mongoose, {Schema} from "mongoose";
 import { MemberStatus, MemberType } from "../libs/types/enums/member.enum";
 
-// Schema first & Code first methodlar bor. Biz  Schema first methodda esa avval schema yaratamiz va undan keyin shu schema asosida model yaratamiz.
+// Schema first & Code first methodlar bor. Biz  Schema first methoddan foydalanamiz Schema firstda avval schema yaratamiz va undan keyin shu schema asosida model yaratamiz.
 // Code first methodni. Code first methodda biz avval modelni yaratamiz va undan keyin shu model asosida schema yaratamiz.
 
 const memberSchema = new Schema({
