11afc934031041d93a635de1ce3f2cbf0789d2d7
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-06-04 02:40:43 +0900

    fix: modules

--- src/libs/types/member.ts
+++ src/libs/types/member.ts
@@ -2,31 +2,31 @@ import { ObjectId } from "mongoose";
 import { MemberStatus, MemberType } from "../enums/member.enum";
 
 export interface Member {
-    _id: Object;
-    memberType: MemberType;
-    memberStatus: MemberStatus;
-    memberNick: string;
-    memberPhone: string;
-    memberPassword?: string;
-    memberImage?: string;
-    memberAddress?: string;
-    memberDesc?: string;
-    memberPoints: number;
-    createdAt: Date;
-    updatedAt: Date;
+  _id: Object;
+  memberType: MemberType;
+  memberStatus: MemberStatus;
+  memberNick: string;
+  memberPhone: string;
+  memberPassword?: string;
+  memberImage?: string;
+  memberAddress?: string;
+  memberDesc?: string;
+  memberPoints: number;
+  createdAt: Date;
+  updatedAt: Date;
 }
 export interface MemberInput {
-    memberType?: MemberType;
-    memberStatus?: MemberStatus;
-    memberNick: string; 
-    memberPhone: string;
-    memberPassword: string;
-    memberImage?: string;
-    memberAddress?: string;
-    memberDesc?: string;
-    memberPoints?: number;
+  memberType?: MemberType;
+  memberStatus?: MemberStatus;
+  memberNick: string;
+  memberPhone: string;
+  memberPassword: string;
+  memberImage?: string;
+  memberAddress?: string;
+  memberDesc?: string;
+  memberPoints?: number;
 }
 export interface LoginInput {
-    memberNick: string;
-    memberPassword: string;
+  memberNick: string;
+  memberPassword: string;
 }
--- src/schema/Member.model.ts
+++ src/schema/Member.model.ts
@@ -1,62 +1,60 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import { MemberStatus, MemberType } from "../libs/enums/member.enum";
 
 // Schema first & Code first methodlar bor. Biz  Schema first methoddan foydalanamiz Schema firstda avval schema yaratamiz va undan keyin shu schema asosida model yaratamiz.
 // Code first methodni. Code first methodda biz avval modelni yaratamiz va undan keyin shu model asosida schema yaratamiz.
 
-const memberSchema = new Schema({
+const memberSchema = new Schema(
+  {
     memberType: {
-        type: String,
-        enum: MemberType,
-        default: MemberType.USER,
+      type: String,
+      enum: MemberType,
+      default: MemberType.USER,
     },
 
-     memberStatus: {
-        type: String,
-        enum: MemberStatus,
-        default: MemberStatus.ACTIVE,
+    memberStatus: {
+      type: String,
+      enum: MemberStatus,
+      default: MemberStatus.ACTIVE,
     },
 
-     memberNick: {
-        type: String,
-        index: {unique: true, sparse: true},
-        required: true,
+    memberNick: {
+      type: String,
+      index: { unique: true, sparse: true },
+      required: true,
     },
 
-     memberPhone: {
-        type: String,
-        index: {unique: true, sparse: true},
-        required: true,
+    memberPhone: {
+      type: String,
+      index: { unique: true, sparse: true },
+      required: true,
     },
 
-     memberPassword: {
-        type: String,
-        select: false, // malumotni olib berma degan shart qo'yyabmiz
-        required: true,
+    memberPassword: {
+      type: String,
+      select: false, // malumotni olib berma degan shart qo'yyabmiz
+      required: true,
     },
 
-     memberImage: {
-        type: String,
-        
+    memberImage: {
+      type: String,
     },
 
-     memberAddress: {
-        type: String,
-       
+    memberAddress: {
+      type: String,
     },
 
-     memberDesc: {
-        type: String,
-        
+    memberDesc: {
+      type: String,
     },
 
-     memberPoints: {
-        type: Number,
-       default: 0,
+    memberPoints: {
+      type: Number,
+      default: 0,
     },
-},
- {timestamps: true} // updatedAt, createAt
-); 
+  },
+  { timestamps: true }, // updatedAt, createAt
+);
 
 // schema modelga aylantirish
-export default mongoose.model('Member', memberSchema);
+export default mongoose.model("Member", memberSchema);
