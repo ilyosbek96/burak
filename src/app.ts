1ba69f7a5157ad20447e5f9b671f416446f2e4f5
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-06-06 20:50:30 +0900

    feat: integrate middlewere sessions

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,15 @@ import router from "./router";
 import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/types/config";
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+import { Collection } from "mongoose";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "session",
+});
 
 /** 1-ENTRANCE kirish bo'limi **/
 const app = express();
@@ -14,7 +23,17 @@ app.use(express.json());
 app.use(morgan(MORGAN_FORMAT)); // (\n hegingi qatorga o'tish degani)
 
 /** 2-SESSIONS **/
-
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 100 * 3600 * 3, // 3 soat devomida active bo'ladi
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
 /** 3-VIEWS **/
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
