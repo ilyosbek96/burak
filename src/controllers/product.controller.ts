8922db7dbabfbbb461fa1be1007dc903aaf0e221
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-06-07 16:37:03 +0900

    feat: create product controller and product routing apis

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import Errors from "../libs/Error";
+import { T } from "../libs/types/common";
+import ProductService from "../models/Product.service";
+
+const productService = new ProductService();
+
+const productController: T = {};
+productController.getAllProducts = async (req: Request, res: Response) => {
+  try {
+    console.log("getAllProducts");
+    res.render("products");
+  } catch (err: any) {
+    console.log("Error, getAllProducts:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+    // res.json({});
+  }
+};
+productController.createNewProduct = async (req: Request, res: Response) => {
+  try {
+    console.log("createNewProduct");
+  } catch (err: any) {
+    console.log("Error, createNewProduct:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+    // res.json({});
+  }
+};
+productController.updateChosenProduct = async (req: Request, res: Response) => {
+  try {
+    console.log("updateChosenProduct");
+  } catch (err: any) {
+    console.log("Error, updateChosenProduct:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+    // res.json({});
+  }
+};
+
+export default productController;
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
+import productController from "./controllers/product.controller";
 
 // router.get("/", (req: Request, res: Response) => {
 //     res.send("Home Page");
@@ -31,5 +32,9 @@ routerAdmin.get("/logout", restaurantController.logout);
 routerAdmin.get("/check_me", restaurantController.checkAuthSession);
 
 /** Product */
+routerAdmin.get("/product/all", productController.getAllProducts);
+routerAdmin.post("/product/create", productController.createNewProduct);
+routerAdmin.post("/product/:id", productController.updateChosenProduct);
+
 /** User */
 export default routerAdmin;
