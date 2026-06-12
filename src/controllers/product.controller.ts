06e5d0a12fb7d50b56235490de89751cddec90b3
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-06-12 22:28:32 +0900

    feat: createNewProduct business logic by retaaurant member

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
-import Errors from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 import { T } from "../libs/types/common";
 import ProductService from "../models/Product.service";
+import { ProductInput } from "../libs/types/product";
+import { AdminRequest } from "../libs/types/member";
 // =============================== test ===========================
 // import { AdminRequest } from "../libs/types/member";
 
@@ -10,6 +12,10 @@ const productService = new ProductService();
 const productController: T = {};
 // =============================== test ===========================
 // productController.getAllProducts = async (req: AdminRequest, res: Response)
+
+/** SPA => SINGLE PAGE APLICATION */
+
+/** SSR */
 productController.getAllProducts = async (req: Request, res: Response) => {
   try {
     console.log("getAllProducts");
@@ -23,14 +29,39 @@ productController.getAllProducts = async (req: Request, res: Response) => {
     // res.json({});
   }
 };
-productController.createNewProduct = async (req: Request, res: Response) => {
+productController.createNewProduct = async (
+  req: AdminRequest,
+  res: Response,
+) => {
   try {
     console.log("createNewProduct");
-    res.send("DONE");
-  } catch (err: any) {
+    // console.log("req.files:", req.files);
+    if (!req.files?.length)
+      throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
+
+    const data: ProductInput = req.body;
+    data.productImages = req.files.map((ele) => {
+      return ele.path;
+    });
+
+    await productService.createNewProduct(data);
+
+    res.send(
+      `<script> alert("Sucessful creation!"); window.location.replace('admin/product/all) </script>`,
+    );
+
+    // console.log("data:", data);
+
+    // res.send("DONE");
+  } catch (err) {
     console.log("Error, createNewProduct:", err);
-    if (err instanceof Errors) res.status(err.code).json(err);
-    else res.status(Errors.standard.code).json(Errors.standard);
+    const message =
+      err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+    res.send(
+      `<script> alert("${message}"); window.location.replace('admin/product/all) </script>`,
+    );
+    // if (err instanceof Errors) res.status(err.code).json(err);
+    // else res.status(Errors.standard.code).json(Errors.standard);
     // res.json({});
   }
 };
