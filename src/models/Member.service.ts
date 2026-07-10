7063968a2ad31c6a2fefa5a22eed782f8b3fd021
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-07-10 23:55:27 +0900

    feat: updateOrder rest Api

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -113,6 +113,21 @@ class MemberService {
     return result;
   }
 
+  public async addUserPoint(member: Member, point: number): Promise<Member> {
+    const memberId = shapeIntoMongooseObjectId(member._id);
+    return await this.memberModel
+      .findByIdAndUpdate(
+        {
+          _id: memberId,
+          memberType: MemberType.USER,
+          memberStatus: MemberStatus.ACTIVE,
+        },
+        { $inc: { memberPoints: point } },
+        { new: true },
+      )
+      .exec();
+  }
+
   //**============================== SSR========================================== */
   // bssr manosi backend server side rendering yani backendda frontendni qurvolish uni EJS ORQALIK QILAMIZ
   /** =================== (BSSR), SSR =================*/
