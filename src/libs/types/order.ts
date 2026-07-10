780b3d9cf630934c1cc89b6e2e86fd77e2183d03
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-07-10 21:46:08 +0900

    feat: develop getMyOrders rest Api

--- src/libs/types/order.ts
+++ src/libs/types/order.ts
@@ -1,5 +1,6 @@
 import { OrderStatus } from "libs/enums/order.enum";
 import { ObjectId } from "mongoose";
+import { Product } from "./product";
 
 export interface OrderItem {
   _id: ObjectId;
@@ -19,6 +20,9 @@ export interface Order {
   memberId: ObjectId;
   createdAt: Date;
   uptadeAr: Date;
+  /** from aggregation */
+  orderItems: OrderItem[];
+  productData: Product[];
 }
 
 export interface OrderItemInput {
@@ -26,4 +30,10 @@ export interface OrderItemInput {
   itemPrice: number;
   productId: ObjectId;
   orderId?: ObjectId;
+}
+
+export interface OrderInquiry {
+  page: number;
+  limit: number;
+  orderStatus: OrderStatus;
 }
--- src/router.ts
+++ src/router.ts
@@ -56,5 +56,10 @@ router.post(
   memberController.verifyAuth,
   orderController.createOrder,
 );
+router.get(
+  "/order/all",
+  memberController.verifyAuth,
+  orderController.getMyOrders,
+);
 
 export default router;
